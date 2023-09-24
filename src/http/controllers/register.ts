6d7696cc658467a9b2895cbe32322ce4744442f4
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2023-09-24 14:33:37 -0300

    style: Add a configuration of eslint

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,23 +1,23 @@
-import { FastifyReply, FastifyRequest } from "fastify"
-import { z } from "zod";
-import { prisma } from "@/lib/prisma";
-import { hash } from "bcryptjs";
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 
-export async function register(request: FastifyRequest, reply: FastifyReply){
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
   })
-  
+
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   const password_hash = await hash(password, 6)
 
   const userWithSameEmail = await prisma.user.findUnique({
-    where:{
-      email
-    }
+    where: {
+      email,
+    },
   })
 
   if (userWithSameEmail) {
@@ -28,7 +28,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply){
     data: {
       name,
       email,
-      password_hash: password_hash,
+      password_hash,
     },
   })
 
