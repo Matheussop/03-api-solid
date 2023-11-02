a8ce22b63622385d6a7c1e010d997b284e98e41f
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2023-11-02 19:46:53 -0300

    feat: use JWT to verify user

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,14 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
   // const authenticateBodySchema = z.object({
   //   email: z.string().email(),
   //   password: z.string().min(6),
@@ -18,5 +25,10 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
   //   throw err
   // }
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
