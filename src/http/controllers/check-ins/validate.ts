2b2a326cf02a5d07ab328c23cb86f3f051099929
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2023-11-09 20:36:02 -0300

    test: Validate Check-in E2E

--- src/http/controllers/check-ins/validate.ts
+++ src/http/controllers/check-ins/validate.ts
@@ -1,14 +1,14 @@
-import { makeCheckInUseCase } from '@/use-cases/factories/make-check-in-use-case'
 import { makeValidateCheckInUseCase } from '@/use-cases/factories/make-validate-check-in-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 export async function validate(request: FastifyRequest, reply: FastifyReply) {
-  const validateCheckInQuerySchema = z.object({
+  console.log('Chequei aqui', request.query)
+  const validateCheckInParamsSchema = z.object({
     checkInId: z.string().uuid(),
   })
 
-  const { checkInId } = validateCheckInQuerySchema.parse(request.query)
+  const { checkInId } = validateCheckInParamsSchema.parse(request.params)
 
   const validadeCheckInUseCase = makeValidateCheckInUseCase()
 
